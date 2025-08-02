40e193621a20669558a6b13011d642ad7e57c119
Ashna Imtiaz <ashnaxhaikh@gmail.com>
2025-08-02 22:16:02 +0200

    Update inverted_triangle.cpp

--- C++/Triangle_patterns/inverted_triangle.cpp
+++ C++/Triangle_patterns/inverted_triangle.cpp
@@ -16,4 +16,27 @@ int main() {
         cout << endl;
     }
     return 0;
+
+}
+
+//homework
+int main() {
+    int n=4;
+    
+    for (int i=0; i<n; i++) {
+        // spaces count 
+        for (int j=0; j<i; j++) {
+            cout << " ";
+        }
+
+        char ch='A'+i;
+        // nums count 
+        for (int j=0; j<n-i; j++) {
+            // nums to be printed
+            // char ch = 'A' + i;
+            cout << ch;
+        }
+        cout << endl;
+    }
+    return 0;
 }
